1b55533ab3d06080813034419f9ca43b86380c49
horiem <yellowshippo@gmail.com>
2021-06-30 03:29:02 +0900

    add scraper

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -14,7 +14,35 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('../femio'))
 
+from glob import glob
+import shutil
+import os
+from sphinx_gallery.scrapers import figure_rst
 from sphinx_gallery.sorting import FileNameSortKey
+
+
+class PNGScraper(object):
+    def __init__(self):
+        self.seen = set()
+
+    def __repr__(self):
+        return 'PNGScraper'
+
+    def __call__(self, block, block_vars, gallery_conf):
+        # Find all PNG files in the directory of this example.
+        path_current_example = os.path.dirname(block_vars['src_file'])
+        pngs = sorted(glob(os.path.join(path_current_example, '*.png')))
+
+        image_names = list()
+        image_path_iterator = block_vars['image_path_iterator']
+        for png in pngs:
+            if png not in self.seen:
+                self.seen |= set(png)
+                this_image_path = image_path_iterator.next()
+                image_names.append(this_image_path)
+                shutil.move(png, this_image_path)
+        # Use the `figure_rst` helper function to generate rST for image files
+        return figure_rst(image_names, gallery_conf['src_dir'])
 
 
 # -- Project information -----------------------------------------------------
@@ -24,7 +52,7 @@ copyright = '2020, RICOS Co. Ltd.'
 author = 'RICOS Co. Ltd.'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.1'
+release = '0.3.2'
 
 
 # -- General configuration ---------------------------------------------------
@@ -43,6 +71,7 @@ sphinx_gallery_conf = {
     'gallery_dirs': 'examples',
     "within_subsection_order": FileNameSortKey,
     "filename_pattern": r"/*\.py",
+    'image_scrapers': ('matplotlib', PNGScraper()),
 }
 
 # Add any paths that contain templates here, relative to this directory.
